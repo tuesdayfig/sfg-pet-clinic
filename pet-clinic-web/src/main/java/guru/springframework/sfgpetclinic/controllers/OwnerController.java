21388fa84956474976ef0a5e492dac8ca6e367c3
Tuesday <dontaskmeaboutit@gmail.com>
2020-05-09 22:12:49 -0400

    lecture 67

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -3,11 +3,11 @@ package guru.springframework.sfgpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/owners")
 @Controller
-public class indexController {
-    @RequestMapping({"", "/", "index", "index.html"})
-    //@RequestMapping("/")
-    public String index(){
-        return "index";
+public class OwnerController {
+    @RequestMapping({"/", "/index", "/index.html"})
+    public String listOwners(){
+        return "owners/index";
     }
 }
