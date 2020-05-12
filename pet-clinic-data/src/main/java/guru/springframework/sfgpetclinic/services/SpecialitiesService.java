ffd553f5f8f2e170c49ebd72c9da6e8ece7aded9
Tuesday <dontaskmeaboutit@gmail.com>
2020-05-12 15:21:22 -0400

    lecture 154 speciality jpa

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialitiesService.java
+++
@@ -1,7 +0,0 @@
-package guru.springframework.sfgpetclinic.services;
-
-import guru.springframework.sfgpetclinic.model.Speciality;
-
-public interface SpecialitiesService extends CrudService<Speciality, Long> {
-
-}
