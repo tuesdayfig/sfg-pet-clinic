944da519863212caa4d14af41fb9cd9dfcacebba
Tuesday <dontaskmeaboutit@gmail.com>
2020-05-12 12:57:35 -0400

    lecture 147

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -9,7 +9,7 @@ public class Person extends BaseEntity {
     @Column(name = "first_name")
     private String firstName;
 
-    @Column(name = "last_night")
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
