5da82a4e54bf3c762f0963c65237b5fc915796e7
Tuesday <dontaskmeaboutit@gmail.com>
2020-05-18 13:35:57 -0400

    lecture 213 fix display owners

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -7,6 +7,7 @@ import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
+
 @Setter
 @Getter
 @NoArgsConstructor
@@ -32,4 +33,5 @@ public class Pet extends BaseEntity{
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
     private Set<Visit> visits = new HashSet<>();
+
 }
